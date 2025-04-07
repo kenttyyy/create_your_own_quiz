6686137f4641fb07c93b09dd9a4a60ee6bce867f
kenttyyy <kentajrins123@gmail.com>
2025-04-07 23:28:14 +0800

    instructions

---
+++ create_your_own_quiz.py
@@ -0,0 +1,4 @@
+# Ask user for a question
+# Ask for 4 possible answer (a,b,c,d) and the correct answer.
+# Write the collected data to a text file.
+# Ask another question until the user chose to exit.
