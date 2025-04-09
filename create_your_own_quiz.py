e2eba4cd328379a8d76fe79e9151405caf59dafc
kenttyyy <kentajrins123@gmail.com>
2025-04-09 18:43:09 +0800

    set variable to input

--- create_your_own_quiz.py
+++ create_your_own_quiz.py
@@ -1,4 +1,12 @@
 # Ask user for a question
+question = input("Enter a question ")
+
 # Ask for 4 possible answer (a,b,c,d) and the correct answer.
+choice_one = input("Enter Choice A ")
+choice_two = input("Enter Choice B ")
+choice_three = input("Enter Choice C ")
+choice_four = input("Enter Choice D ")
+correct_answer = input("Enter the correct answer ")
+
 # Write the collected data to a text file.
 # Ask another question until the user chose to exit.
